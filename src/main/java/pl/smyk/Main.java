9c4bf7c44953a15a8e4840857f0998dfd3e8dd0a
Kamil Smyk <kamil.smyk00@gmail.com>
2023-03-06 19:44:04 +0100

    fix

--- src/main/java/pl/smyk/Main.java
+++ src/main/java/pl/smyk/Main.java
@@ -9,15 +9,14 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String csvFileName = "C:\\java\\lab1\\src\\main\\resources\\kody.csv";
-        String dbUrl = "jdbc:mysql://localhost:3306/mydb";
+        String csvFileName = "C:\\Users\\Kamil\\Desktop\\kody.csv";
+        String dbUrl = "jdbc:mysql://localhost:3306/testdb";
         String username = "root";
         String password = "admin";
         Connection connection = null;
 
         try {
             connection = DriverManager.getConnection(dbUrl, username, password);
-            connection.setAutoCommit(false);
 
             String createTableSQL = "CREATE TABLE IF NOT EXISTS kody (id INT PRIMARY KEY AUTO_INCREMENT, post_code VARCHAR (255), adress VARCHAR(255), voivoship VARCHAR(255), county VARCHAR(255))";
             connection.createStatement().execute(createTableSQL);
@@ -32,6 +31,7 @@ public class Main {
 
             reader.readLine();
 
+            long startTime = System.currentTimeMillis() / 1000; // początek procedury zapisu danych
             while ((record = reader.readLine()) != null) {
                 String[] data = record.split(";");
                 String postCode = data[0];
@@ -43,13 +43,15 @@ public class Main {
                 statement.setString(2, adress);
                 statement.setString(3, voivoship);
                 statement.setString(4, county);
-                statement.addBatch();
+
+                statement.executeUpdate();
             }
-            statement.executeBatch();
+
             connection.commit();
             connection.close();
-
-            System.out.println("Records inserted successfully!");
+            long endTime = System.currentTimeMillis() / 1000; // koniec procedury zapisu danych oraz połączenie z bazą zostaje zamknięte
+            long duration = endTime - startTime;
+            System.out.println("Czas zapisu danych do bazy wynosi: " + duration + "s");
 
 
         } catch (SQLException e) {
